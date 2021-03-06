6595ae4f97d1a075c26ca82ee9f3b04185b8cd54
yourankim <youran.dev@gmail.com>
2021-03-06 21:55:13 +0900

    Update: 350. Intersection of Two Arrays II

--- problems/350. Intersection of Two Arrays II/index.js
+++ problems/350. Intersection of Two Arrays II/index.js
@@ -24,3 +24,35 @@ var intersect = function (nums1, nums2) {
   }
   return result;
 };
+
+/**
+ *
+ * 2. sort 이용: 84ms, 39.5MB
+ * 문제에서 제시된 첫번째 follow up
+ * "What if the given array is already sorted? How would you optimize your algorithm?"
+ * 에 따라 주어진 배열을 정렬하여 풀어보았다.
+ * 자바스크립트의 Array.sort()는 기본적으로 요소를 문자열로 변환하여 정렬한다.
+ * 따라서 숫자 기준으로 정렬하려면 compareFunction를 정의해 아규먼트로 넘겨야 한다.
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  const result = [];
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  let i1 = 0,
+    i2 = 0;
+  while (i1 < nums1.length && i2 < nums2.length) {
+    if (nums1[i1] < nums2[i2]) {
+      i1++;
+    } else if (nums1[i1] > nums2[i2]) {
+      i2++;
+    } else {
+      result.push(nums1[i1]);
+      i1++;
+      i2++;
+    }
+  }
+  return result;
+};
